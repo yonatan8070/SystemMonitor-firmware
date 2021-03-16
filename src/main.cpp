f57fb14ba10b08ca6f9a0d37fcd907438047b7b2
yonatan3058@outlook.com <yonatan3058@outlook.com>
2021-03-16 21:03:58 +0200

    Move to the new Display and start rewriting the update functionallity

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,75 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <SPI.h>
 #include <SerialCommand.h>
 #include <U8g2lib.h>
-#include <SPI.h>
 #include <Wire.h>
 
-U8G2_ST7920_128X64_F_HW_SPI display(U8G2_R0, 12);
-LiquidCrystal_I2C lcd(0x27, 20, 4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+U8G2_ST7920_128X64_F_HW_SPI display(U8G2_R2, 12);
 SerialCommand sCmd;
+
+const char *cpuTemp;
+const char *cpuUsage;
+const char *gpuTemp;
+const char *gpuUsage;
 
 void loop() {
   sCmd.readSerial();
 }
 
-void cpuTemp() {
+void writeToDisplay() {
+  display.setCursor(0, 11);
+
+  display.print("CPU: ");
+  display.print(cpuUsage);
+  display.print("% @ ");
+  display.print(cpuTemp);
+  display.print("°");
+
+  
+  
+  
+  display.sendBuffer();
+}
+
+void cpuTempCmd() {
   char *arg = sCmd.next();
   if (arg != NULL) {
     Serial.println(arg);
-    lcd.setCursor(0, 0);
-    lcd.print("CPU: ");
-    lcd.print(arg);
+    cpuTemp = arg;
+    writeToDisplay();
   } else {
     Serial.println("Error");
   }
 }
 
-void gpuTemp() {
+void gpuTempCmd() {
   char *arg = sCmd.next();
   if (arg != NULL) {
     Serial.println(arg);
-    lcd.setCursor(9, 0);
-    lcd.print("GPU: ");
-    lcd.print(arg);
+    display.drawStr(0, 11, arg);
+    display.sendBuffer();
   } else {
     Serial.println("Error");
   }
 }
 
-void cpuUsage() {
+void cpuUsageCmd() {
   char *arg = sCmd.next();
   if (arg != NULL) {
     Serial.println(arg);
-    lcd.setCursor(0, 1);
-    lcd.print("CPU: ");
-    lcd.print(arg);
+    cpuUsage = arg;
   } else {
     Serial.println("Error");
   }
 }
 
-void gpuUsage() {
+void gpuUsageCmd() {
   char *arg = sCmd.next();
   if (arg != NULL) {
     Serial.println(arg);
-    lcd.setCursor(9, 1);
-    lcd.print("GPU: ");
-    lcd.print(arg);
+    display.drawStr(0, 11, arg);
+    display.sendBuffer();
   } else {
     Serial.println("Error");
   }
@@ -66,17 +80,16 @@ void unrecognized(const char *command) {
 }
 
 void setup() {
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-
   display.begin();
+  display.clear();
+  display.setFont(u8g2_font_t0_11_mf);
+  display.enableUTF8Print();
 
   Serial.begin(9600);
 
-  sCmd.addCommand("ct", cpuTemp);
-  sCmd.addCommand("gt", gpuTemp);
-  sCmd.addCommand("cu", cpuUsage);
-  sCmd.addCommand("gu", gpuUsage);
+  sCmd.addCommand("ct", cpuTempCmd);
+  sCmd.addCommand("gt", gpuTempCmd);
+  sCmd.addCommand("cu", cpuUsageCmd);
+  sCmd.addCommand("gu", gpuUsageCmd);
   sCmd.setDefaultHandler(unrecognized);  // Handler for command that isn't matched  (says "What?")
 }
